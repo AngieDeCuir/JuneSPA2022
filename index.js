27c489e9201186b46f4da4e1a5f8e3a490817de6
Angie DeCuir <adecuir8@gmail.com>
2022-07-12 20:12:58 -0500

    JuneSPA2022

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import * as components from "./components";
+function render() {
+  document.querySelector()
+    ${Nav ()}
+
+
+    `
+
+
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
