3ae8da11e4cc47497a84627f7b89fa8e82b7ac17
Angie DeCuir <adecuir8@gmail.com>
2022-08-02 16:52:13 -0500

    Changes made in 8/1/22

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
-// Connect to database order matters be specific
+// Connect to the database
 mongoose.connect(process.env.MONGODB);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
@@ -20,11 +20,27 @@ db.once(
   console.log.bind(console, "Successfully opened connection to Mongo!")
 );
 
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 
